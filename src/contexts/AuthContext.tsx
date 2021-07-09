fc3334b1b9fa9ca8866081e72330507955050733
Fillipi Pereira <wenanzer@gmx.de>
2021-07-09 18:47:06 +0200

    Start

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,15 +1,15 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { auth, firebase } from "../services/firebase";
+import axios from 'axios';
 
-type User = {
+export type User = {
   id: string;
   name: string;
-  avatar: string;
 }
 
 type AuthContextType = {
   user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
+  setUser: (user: User) => void;
+  getValidUser: () => Promise<User[]>;
 }
 
 type AuthContextProviderProps = {
@@ -21,50 +21,14 @@ export const AuthContext = createContext({} as AuthContextType);
 export function AuthContextProvider(props: AuthContextProviderProps) {
   const [user, setUser] = useState<User>();
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      if (user) {
-        const { displayName, photoURL, uid } = user
-
-        if (!displayName || !photoURL) {
-          throw new Error('Missing information from Google Account.');
-        }
-
-        setUser({
-          id: uid,
-          name: displayName,
-          avatar: photoURL
-        })
-      }
-    })
-
-    return () => {
-      unsubscribe();
-    }
-  }, [])
-
-  async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider)
-
-    if (result.user) {
-      const { displayName, photoURL, uid } = result.user
-
-      if (!displayName || !photoURL) {
-        throw new Error('Missing information from Google Account.');
-      }
-
-      setUser({
-        id: uid,
-        name: displayName,
-        avatar: photoURL
-      })
-    }
+  async function getValidUser() {
+    const response = await axios.get("http://assignment.bunq.com/users")
+    return response.data
   }
 
+
   return (
-    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+    <AuthContext.Provider value={{ user, setUser, getValidUser }}>
       {props.children}
     </AuthContext.Provider>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,46 @@
-import { useContext } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom'
 
-import { AuthContext } from '../contexts/AuthContext';
-
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/llogo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+import { User } from '../contexts/AuthContext';
 
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext)
+  const { user, setUser, getValidUser } = useAuth();
+  const [validUsers, setValidUsers] = useState<User[]>([]);
+  useEffect(() => { loadValidUser() }, [])
 
-  async function handleCreateRoom() {
-    if (!user) {
-      await signInWithGoogle()
-    }
+  getValidUser().then(console.log)
 
-    history.push('/rooms/new');
+  async function handleJoinChat(event: FormEvent) {
+    event.preventDefault();
+    history.push('/rooms/new')
+  }
+
+  async function loadValidUser() {
+    const userData = await getValidUser()
+    setValidUsers(userData)
   }
 
   return (
     <div id="page-auth">
-      <aside>
-        <img src={illustrationImg} alt="Ilustration simbolizing questions and answers" />
-        <strong>Create your live questions &amp; answers room</strong>
-        <p>Answer your audicence's questions in real time</p>
-      </aside>
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Lamegspørre" />
-          <button onClick={handleCreateRoom} className="create-room">
-            <img src={googleIconImg} alt="Google-logo" />
-            Log with your Google account.
-          </button>
-          <div className="separator">or enter in a room</div>
-          <form>
-            <input
-              type="text"
-              placeholder="Room code"
-            />
-            <Button type="submit">
-              Enter in a room
-            </Button>
+          <h2>Who are you?</h2>
+          <form onSubmit={handleJoinChat}>
+            {
+              validUsers && validUsers.map(user => (
+                <Button onClick={() => setUser(user)} className="button outlined" key={user.id} type="submit">
+                  {user.name}
+                </Button>
+              ))
+            }
           </form>
         </div>
       </main>
